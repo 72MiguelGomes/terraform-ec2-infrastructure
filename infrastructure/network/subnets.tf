8ed6f186ea6e5115b8010fc9407efeda4f8008d8
miguelgomes <jmbg.41@gmail.com>
2018-07-24 08:28:41 +0100

    DMZ Subnet Definition

--- infrastructure/network/subnets.tf
+++ infrastructure/network/subnets.tf
@@ -1,33 +1,70 @@
 # Private Subnet (does not have any access to internet)
 resource "aws_subnet" "private_a" {
-  vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "${data.aws_region.current.name}a"
 
   tags {
-    Name = "${var.environment} Private Subnet A"
+    Name      = "${var.environment} Private Subnet A"
+    Terraform = "true"
   }
 }
 
 resource "aws_subnet" "private_b" {
-  vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "${data.aws_region.current.name}b"
 
   tags {
-    Name = "${var.environment} Private Subnet B"
+    Name      = "${var.environment} Private Subnet B"
+    Terraform = "true"
   }
 }
 
 resource "aws_subnet" "private_c" {
-  vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "${data.aws_region.current.name}c"
 
   tags {
-    Name = "${var.environment} Private Subnet C"
+    Name      = "${var.environment} Private Subnet C"
+    Terraform = "true"
   }
 }
+
 # DMZ Subnet (Have access to internet using a nat)
+resource "aws_subnet" "dmz_a" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags {
+    Name      = "${var.environment} DMZ Subnet A"
+    Terraform = "true"
+  }
+}
+
+resource "aws_subnet" "dmz_b" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags {
+    Name      = "${var.environment} DMZ Subnet B"
+    Terraform = "true"
+  }
+}
+
+resource "aws_subnet" "dmz_c" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "${data.aws_region.current.name}c"
+
+  tags {
+    Name      = "${var.environment} DMZ Subnet C"
+    Terraform = "true"
+  }
+}
 
 # Public Subnet (Have direct acess to the internet, ALBs)
+
